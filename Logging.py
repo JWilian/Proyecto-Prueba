3b5709cd9ffbf822e2e50e427a024acccdc526b4
Jorge_Wilianders <wilianders.uni@gmail.com>
2022-10-24 16:44:23 -0500

    "importar logging"

---
+++ Logging.py
@@ -0,0 +1,7 @@
+import logging
+#NIVELES DE REGISTRO - Depuración, información, advertencia, error y critical
+logging.debug("debug") #Depuración.
+logging.info("info")  #Información
+logging.warning("warning")   #Advertencia
+logging.error("error") #Error
+logging.critical("critical")  #Critical
--- datatype.py
+++
@@ -1,4 +0,0 @@
-print("Hola Mundo")
-print(1+5)
-a=10+5.0
-print(type(a))
